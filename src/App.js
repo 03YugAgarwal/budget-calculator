5f8026c6e0b9e819361f10c66994ea7ffce209bd
Yug <03yugagarwal@gmail.com>
2022-12-10 09:35:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,130 @@
+import {useState , useEffect} from 'react'
+import './App.css';
+
+import ExpenseList from './components/ExpenseList'
+import Alert from './components/Alert'
+import ExpenseForm from './components/ExpenseForm'
+
+import { v4 as uuidv4 } from 'uuid';
+
+// const initialCost = [
+//   {id:uuidv4(),charge:"Rent",amount:1600},
+//   {id:uuidv4(),charge:"Car Payment",amount:2000},
+//   {id:uuidv4(),charge:"Electricity",amount:200},
+// ]
+
+const initialCost = localStorage.getItem('expenses')? JSON.parse(localStorage.getItem("expenses")) : []
+
+function App() {
+
+  // State Values
+  const [expenses, setExpenses] = useState(initialCost)
+
+  const [charge, setCharge] = useState('')
+  
+  const [amount, setAmount] = useState('')
+
+  const [alert, setAlert] = useState({show: false})
+
+  const [edit, setEdit] = useState(false)
+
+  const [id, setId] = useState(0)
+
+  useEffect(() => {
+    localStorage.setItem('expenses',JSON.stringify(expenses))
+  
+  }, [expenses])
+  
+  
+  // functionality
+
+  const handleCharge = e =>{
+    setCharge(e.target.value)
+  }
+  const handleAmount = e =>{
+    setAmount(e.target.value)
+  }
+  const handleSubmit = e =>{
+    e.preventDefault();
+    console.log(charge,amount);
+
+    if(charge !== '' && amount !==''){
+      if(edit){
+        let tempExpenses = expenses.map(item =>{
+          return item.id === id? {...item,charge,amount} :item
+        })
+        setExpenses(tempExpenses)
+        setEdit(false)
+        handleAlert({type:'success',text:'item edited'})
+
+      }else{
+        const singleExpense = {id: uuidv4(),charge, amount};
+        setExpenses([...expenses,singleExpense])
+        handleAlert({type:'success',text:'item added'})
+      }
+      setAmount('')
+      setCharge('')
+
+    }else{
+      handleAlert({type:'danger',text:`charge can't be empty and the amount should have some value`})
+    }
+
+  }
+
+  const handleAlert = ({type,text}) => {
+    setAlert({show:true,type,text})
+    setTimeout(()=>{
+      setAlert({show:false})
+    },3000)
+  }
+
+  // Clear all items
+  const clearItems = () => {
+    setExpenses([])
+    handleAlert({type: "danger", text:"all items deleted"})
+  }
+
+  const handleDelete = (id) => {
+    let tempExpenses = expenses.filter(item => item.id !== id)
+    setExpenses(tempExpenses)
+    handleAlert({type: "danger", text:"item deleted"})
+  }
+
+  const handleEdit = (id) => {
+    let expense = expenses.find(item => item.id === id)
+    let {charge,amount} = expense
+    setCharge(charge)
+    setAmount(amount)
+    setEdit(true)
+    setId(id) 
+  }
+
+  return (
+    <>
+      {alert.show && <Alert type={alert.type} text={alert.text}/>}
+      
+      <h1>Budget Calculator</h1>
+      <main className="App">
+        <ExpenseForm 
+        charge={charge} 
+        amount={amount} 
+        handleAmount={handleAmount} 
+        handleCharge={handleCharge} 
+        handleSubmit={handleSubmit}
+        edit={edit}
+        />
+        <ExpenseList 
+        expenses={expenses} 
+        handleDelete={handleDelete}
+        handleEdit={handleEdit} 
+        clearItems={clearItems} 
+        />
+      </main>
+      <h1>total spended: <span className="total">${expenses.reduce((acc,curr)=>{
+        return acc += parseInt(curr.amount)
+      },0)}</span></h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ExpenseItem from './ExpenseItem'
+import {MdDelete} from 'react-icons/md'
+
+const ExpenseList = ({expenses,handleEdit,handleDelete,clearItems}) => {
+  return (
+    <>
+        <ul className='list'>
+            {expenses.map((expense)=>{
+                return <ExpenseItem key={expense.id} expense={expense} handleDelete={handleDelete}
+                handleEdit={handleEdit}/>
+            })}
+        </ul>
+        {expenses.length > 0 && 
+        <button className='btn' onClick={clearItems}>clear<MdDelete className='btn-icons'/> </button>}
+    </>
+  )
+}
+
+export default ExpenseList
